f1f30c73bf8dc3a3ee3b1a8f5d3f3b0c711d0003
rathiaarzoo <72386964+rathiaarzoo@users.noreply.github.com>
2023-07-17 13:45:06 +0530

    Create OptionalExample.java

---
+++ OptionalExample.java
@@ -0,0 +1,10 @@
+//Not using optional class in this program so it will terminates abnormally throws a nullPointerException 
+public class OptionalExample { 
+ public static void main(String[] args) { 
+ String[] str = new String[10]; 
+ String lowercaseString = str[5].toLowerCase(); 
+ System.out.print(lowercaseString); 
+ } 
+} 
+
+
