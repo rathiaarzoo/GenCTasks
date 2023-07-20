622424d71b92eba2afacb33a083dabfb89dd0d3c
rathiaarzoo <72386964+rathiaarzoo@users.noreply.github.com>
2023-07-20 12:00:02 +0530

    Update Item.java

---
+++ Item.java
@@ -0,0 +1,12 @@
+//Examples to ‘group by’ a list of user defined Objects.(GroupingBy3 and GroupingBy4)
+//A POJO
+import java.math.BigDecimal;
+
+public class Item {
+
+    private String name;
+    private int qty;
+    private BigDecimal price;
+
+    //constructors, getter/setters 
+}
